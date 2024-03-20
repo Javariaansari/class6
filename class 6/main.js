ef01496afdfb94bbed7f919855e849faee489f34
Javariaansari <157297608+Javariaansari@users.noreply.github.com>
2024-03-20 13:14:57 -0700

    Add files via upload

---
+++ class 6/main.js
@@ -0,0 +1,38 @@
+// function halffryEgg(){
+//    let cooked = 1+ 1.5 +2; 
+// //    console.log(cooked);
+//  return cooked
+// }
+//  const egg = halffryEgg()
+//  console.log(egg);
+//  function halffryFish(){
+//     let cooked = 1+ 1.5 +2; 
+//  //    console.log(cooked);
+// //   return cooked
+//  }
+//   const fish = halffryFish()
+//   console.log(fish);
+//   function halffrymeat(){
+//     let cooked = 1+ 1.5 +2; 
+//     console.log(cooked);
+// //   return cooked
+//  }
+//   const response = halffrymeat()
+//   console.log(response);
+//   function halffryEgg():string{
+//     const cooked = "Hello + ubaid"
+//     // console.log(cooked);
+//       return cooked
+//   }
+//     const response = halffryEgg()
+//   console.log(response);
+function halffryEgg(egg, butter, salt) {
+    return egg + butter + salt;
+}
+var response = halffryEgg(1, 1.5, 2);
+console.log(response);
+function halffrymeat(javaria, sharmeen) {
+    return javaria + sharmeen;
+}
+var fun1 = halffrymeat("book", "blue");
+console.log(fun1);
---
+++ class 6/main.ts
@@ -0,0 +1,45 @@
+// function halffryEgg(){
+//    let cooked = 1+ 1.5 +2; 
+// //    console.log(cooked);
+//  return cooked
+// }
+//  const egg = halffryEgg()
+//  console.log(egg);
+
+//  function halffryFish(){
+//     let cooked = 1+ 1.5 +2; 
+//  //    console.log(cooked);
+// //   return cooked
+//  }
+//   const fish = halffryFish()
+//   console.log(fish);
+
+//   function halffrymeat(){
+//     let cooked = 1+ 1.5 +2; 
+//     console.log(cooked);
+// //   return cooked
+//  }
+//   const response = halffrymeat()
+//   console.log(response);
+
+//   function halffryEgg():string{
+//     const cooked = "Hello + ubaid"
+//     // console.log(cooked);
+//       return cooked
+//   }
+//     const response = halffryEgg()
+//   console.log(response);
+
+function halffryEgg(egg:number, butter: number, salt:number ):any {
+return egg + butter + salt
+}
+let response:number = halffryEgg(1, 1.5, 2)
+console.log(response)
+
+function halffrymeat(javaria:string, sharmeen:string ):any {
+    return javaria + sharmeen
+    }
+    let fun1:string = halffrymeat( "book","blue")
+    console.log(fun1)
+
+
